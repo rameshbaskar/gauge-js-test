c43c1ab99351f4cb72c7f38a28e9d12f369ce043
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2020-03-24 13:06:20 +0530

    Removed the waitFor and tried a different way to implement search

--- step_implementations/home_page_steps.js
+++ step_implementations/home_page_steps.js
@@ -1,16 +1,14 @@
-const { write, click, goto, into, highlight, waitFor } = require('taiko');
+const { write, click, goto, into, highlight, text } = require('taiko');
 
 // page objects
 const txtSearchField = "$[name='q']";
-const btnSearch = "Google Search";
 
 step("Goto Home Page", async () => {
-    await goto('http://www.google.co.in/');
+    await goto('http://www.google.com/');
 });
 
 step("Search for <textToSearch>", async (textToSearch) => {
     await write(textToSearch, into(txtSearchField));
-    await waitFor(3000);
-    await highlight(btnSearch);
-    await click(btnSearch);
+    await highlight(text(textToSearch));
+    await click(text(textToSearch));
 });
